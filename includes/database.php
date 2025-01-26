c573587bcaa314bc20ce5182b9b1104d52ae3b22
Yen Luong <yl3434@drexel.edu>
2025-01-25 19:04:56 -0500

    🚧 add database

---
+++ includes/database.php
@@ -0,0 +1,21 @@
+<?php
+$db_server = 'localhost';
+$db_username = 'root';
+$db_password = 'root';
+$db_name = 'idm216';
+
+// $db_server = getenv('DB_SERVER');
+// $db_username = getenv('DB_USERNAME');
+// $db_password = getenv('DB_PASSWORD');
+// $db_name = getenv('DB_NAME');
+
+// Establish a connection to the database
+$connection = new mysqli($db_server, $db_username, $db_password, $db_name);
+
+// Check for connection errors
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+}
+
+// echo "Database connected successfully!";
+?>
