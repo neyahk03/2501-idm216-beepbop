533aad0b0a8151bf72ad37931d21feb55992a882
Yen Luong <yl3434@drexel.edu>
2025-01-26 00:06:53 -0500

    💅 Update css for record.php
    Change padding and add hover state for each row

--- record.php
+++ record.php
@@ -39,6 +39,7 @@ $items = $menuStatement->get_result()->fetch_all(MYSQLI_ASSOC);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Beepbop database</title>
     <link rel="stylesheet" href="css/record.css">
+    <link rel="icon" type="image/gif" href="images/petes-lunchbox-logo.svg" />
 </head>
 
 <body>
