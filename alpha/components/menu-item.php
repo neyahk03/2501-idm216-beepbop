afcc79c39a4afcfdbe77e86949eb4b12e8e43ce2
Yen Luong <yl3434@drexel.edu>
2025-02-09 01:13:53 -0500

    🚧 Created menu.php for final prototype

---
+++ alpha/components/menu-item.php
@@ -0,0 +1,247 @@
+
+        <div class="menu-container">
+
+            <div class="title">
+                <div class="line"></div>
+                <h2>Sandwiches</h2>
+                <div class="line"></div>
+            </div>
+
+            <div class="category">
+
+
+                <div class="item">
+                    <img src="../images/menu-item/sandwiches/egg-and-cheese.jpg" alt="egg and cheese image">
+                    <div class="description">
+                        <p class="menu-title">Egg & Cheese</p>
+                        <p class="price">$4.50</p>
+                    </div>
+                </div>
+    
+                <div class="item">
+                    <img src="../images/menu-item/sandwiches/grilled-cheese.jpg" alt="egg and cheese image">
+                    <div class="description">
+                        <p class="menu-title">Grilled Cheese</p>
+                        <p class="price">$4.50</p>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <img src="../images/menu-item/sandwiches/bacon-lettuce-tomato.jpg" alt="egg and cheese image">
+                    <div class="description">
+                        <p class="menu-title">BLT Sandwich</p>
+                        <p class="price">$4.50</p>
+                    </div>
+                </div>
+            
+            </div>
+
+            <div class="title">
+                <div class="line"></div>
+                <h2>Cheese Steaks</h2>
+                <div class="line"></div>
+            </div>
+
+
+            <div class="category">
+
+                <div class="item">
+                    <img src="../images/menu-item/steak/cheese-steak.jpg " alt="egg and cheese image">
+                    <div class="description">
+                        <p class="menu-title">Cheese Steak</p>
+                        <p class="price">$4.50</p>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <img src="../images/menu-item/steak/buffalo-chicken-cheese-steak.jpg " alt="egg and cheese image">
+                    <div class="description">
+                        <p class="menu-title">Buffalo Chicken Steak</p>
+                        <p class="price">$4.50</p>
+                    </div>
+                </div>
+    
+                
+                <div class="item">
+                    <img src="../images/menu-item/steak/mushroom-cheese-steak.jpg" alt="egg and cheese image">
+                    <div class="description">
+                        <p class="menu-title">Mushroom Chese Steak</p>
+                        <p class="price">$4.50</p>
+                    </div>
+                </div>
+    
+                <div class="item">
+                    <img src="../images/menu-item/steak/pepperoni-cheese-steak.jpg" alt="egg and cheese image">
+                    <div class="description">
+                        <p class="menu-title">Pepperoni Cheese Steak</p>
+                        <p class="price">$4.50</p>
+                    </div>
+                </div>
+    
+                <div class="item">
+                    <img src="../images/menu-item/steak/pizza-steak.jpg" alt="egg and cheese image">
+                    <div class="description">
+                        <p class="menu-title">Pizza Steak</p>
+                        <p class="price">$4.50</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="title">
+                <div class="line"></div>
+                <h2>Salads</h2>
+                <div class="line"></div>
+            </div>
+
+            <div class="category">
+
+                <div class="item">
+                    <img src="../images/menu-item/salad/garden-salad.jpg" alt="garden salad">
+                    <div class="description">
+                        <p class="menu-title">Garden Salad</p>
+                        <p class="price">$4.50</p>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <img src="../images/menu-item/salad/chef-salad.jpg" alt="Chef Salad image">
+                    <div class="description">
+                        <p class="menu-title">Chef Salad</p>
+                        <p class="price">$4.50</p>
+                    </div>
+                </div>
+    
+                
+                <div class="item">
+                    <img src="../images/menu-item/salad/chicken-salad.jpg" alt="Chicken Salad image">
+                    <div class="description">
+                        <p class="menu-title">Chicken Salad</p>
+                        <p class="price">$4.50</p>
+                    </div>
+                </div>
+    
+                <div class="item">
+                    <img src="../images/menu-item/salad/grilled-chicken-salad.jpg" alt="Grilled Chickedn Salad image">
+                    <div class="description">
+                        <p class="menu-title">Grilled Chickedn Salad</p>
+                        <p class="price">$4.50</p>
+                    </div>
+                </div>
+    
+                <div class="item">
+                    <img src="../images/menu-item/salad/tuna-salad.jpg" alt="Tuna Salad image">
+                    <div class="description">
+                        <p class="menu-title">Tuna Salad</p>
+                        <p class="price">$4.50</p>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="title">
+                <div class="line"></div>
+                <h2>Pastries</h2>
+                <div class="line"></div>
+            </div>
+
+
+            <div class="category">
+
+                <div class="item">
+                    <img src="../images/menu-item/pastry/bagel-and-cream-cheese.jpg" alt="Bagel w/ Cream Cheese">
+                    <div class="description">
+                        <p class="menu-title">Bagel w/ Cream Cheese</p>
+                        <p class="price">$4.50</p>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <img src="../images/menu-item/pastry/muffin.jpg" alt="Muffin">
+                    <div class="description">
+                        <p class="menu-title">Muffin</p>
+                        <p class="price">$4.50</p>
+                    </div>
+                </div>
+    
+                
+                <div class="item">
+                    <img src="../images/menu-item/pastry/chips.jpg" alt="Chips">
+                    <div class="description">
+                        <p class="menu-title">Chips</p>
+                        <p class="price">$4.50</p>
+                    </div>
+                </div>
+    
+                <div class="item">
+                    <img src="../images/menu-item/pastry/hashbrown.png" alt="Hashbrown">
+                    <div class="description">
+                        <p class="menu-title">Hashbrown</p>
+                        <p class="price">$4.50</p>
+                    </div>
+                </div>
+    
+            </div>
+
+            <div class="title">
+                <div class="line"></div>
+                <h2>Drinks</h2>
+                <div class="line"></div>
+            </div>
+
+
+            <div class="category">
+
+                <div class="item">
+                    <img src="../images/menu-item/drink/hot-coffee.jpg" alt="Hot Coffee">
+                    <div class="description">
+                        <p class="menu-title">Hot Coffee</p>
+                        <p class="price">$4.50</p>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <img src="../images/menu-item/drink/hot-tea.jpg" alt="Hot Tea">
+                    <div class="description">
+                        <p class="menu-title">Hot Tea</p>
+                        <p class="price">$4.50</p>
+                    </div>
+                </div>
+    
+                
+                <div class="item">
+                    <img src="../images/menu-item/drink/hot-chocolate.jpg" alt="Hot Chocolate">
+                    <div class="description">
+                        <p class="menu-title">Hot Chocolate</p>
+                        <p class="price">$4.50</p>
+                    </div>
+                </div>
+    
+                <div class="item">
+                    <img src="../images/menu-item/drink/soda.png" alt="Soda">
+                    <div class="description">
+                        <p class="menu-title">Soda</p>
+                        <p class="price">$4.50</p>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <img src="../images/menu-item/drink/water.jpg" alt="Water">
+                    <div class="description">
+                        <p class="menu-title">Water</p>
+                        <p class="price">$4.50</p>
+                    </div>
+                </div>
+                
+                <div class="item">
+                    <img src="../images/menu-item/drink/thai-tea.jpg" alt="Thai Tea">
+                    <div class="description">
+                        <p class="menu-title">Thai Tea</p>
+                        <p class="price">$4.50</p>
+                    </div>
+                </div>
+    
+            </div>
+
+            
+    
+            
