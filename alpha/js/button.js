7a22214805631499581254a0606b0083a71175b3
Yen Luong <yl3434@drexel.edu>
2025-02-10 21:33:05 -0500

    🚧 Add check-out and log-in screen

---
+++ alpha/js/button.js
@@ -0,0 +1,11 @@
+function goToOrderStatus() {
+    window.location.href = "order-status.html";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Script loaded");
+});
+
+function viewOrderStatus() {
+    window.location.href = "order-status.html"
+}
