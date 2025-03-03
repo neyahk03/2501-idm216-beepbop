08989c71e742e780bd2cc7d2d6c89baaef5b3331
Yen Luong <yl3434@drexel.edu>
2025-03-03 14:18:55 -0500

    🐛 Update fav section

--- final/functions/remove.php
+++ final/functions/remove.php
@@ -1,58 +1,4 @@
 <?php
-// session_start();
-// header('Content-Type: application/json'); // Ensure JSON response
-
-// $response = ["success" => false, "bag_subtotal" => 0];
-
-// if (isset($_POST['index'])) {
-//     $index = intval($_POST['index']);
-//     if (isset($_SESSION['cart'][$index])) {
-//         unset($_SESSION['cart'][$index]);
-//         $_SESSION['cart'] = array_values($_SESSION['cart']); // Reindex array
-
-//         // Recalculate subtotal
-//         $subtotal = 0;
-//         foreach ($_SESSION['cart'] as $item) {
-//             $subtotal += $item['price'] * $item['quantity'];
-//         }
-
-//         $response["success"] = true;
-//         $response["bag_subtotal"] = $subtotal;
-//     }
-// }
-
-// echo json_encode($response); // Send JSON response
-// exit();
-
-// session_start();
-// header('Content-Type: application/json'); // Ensure JSON response
-
-// $response = ["success" => false, "bag_subtotal" => 0, "total_quantity" => 0];
-
-// if (isset($_POST['index'])) {
-//     $index = intval($_POST['index']);
-    
-//     if (isset($_SESSION['cart'][$index])) {
-//         unset($_SESSION['cart'][$index]);
-//         $_SESSION['cart'] = array_values($_SESSION['cart']); // Reindex array
-
-//         // Recalculate subtotal & total quantity
-//         $subtotal = 0;
-//         $totalQuantity = 0;
-
-//         foreach ($_SESSION['cart'] as $item) {
-//             $subtotal += $item['price'] * $item['quantity'];
-//             $totalQuantity += $item['quantity'];
-//         }
-
-//         $response["success"] = true;
-//         $response["bag_subtotal"] = $subtotal;
-//         $response["total_quantity"] = $totalQuantity;
-//     }
-// }
-
-// echo json_encode($response); // Send JSON response
-// exit();
 
 session_start();
 header('Content-Type: application/json'); // Ensure JSON response
@@ -64,7 +10,7 @@ if (isset($_POST['index'])) {
     
     if (isset($_SESSION['cart'][$index])) {
         unset($_SESSION['cart'][$index]);
-        $_SESSION['cart'] = array_values($_SESSION['cart']); // Reindex array
+        $_SESSION['cart'] = array_values($_SESSION['cart']);
 
         // Recalculate subtotal & total quantity
         $subtotal = 0;
